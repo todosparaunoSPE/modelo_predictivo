e5a1b00c7282afcd366375a538278269e3e4b54a
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2025-02-11 14:21:58 -0600

    Update rendimiento.py

--- rendimiento.py
+++ rendimiento.py
@@ -15,6 +15,24 @@ from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
 from sklearn.preprocessing import StandardScaler
 
+
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
 # Cargar datos desde el archivo .xlsx
 @st.cache_data
 def cargar_datos():
